28fccf199922cbb7c11c4133226230e30ce0f578
Corentin VALET <cocotereg@gmail.com>
2023-02-10 16:04:32 +0100

    fix : add first page v0.5

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace easysave
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
