1a2028368daef3694389cf8e6503bef4993459d9
ElFabinou <fabien.ribes@viacesi.fr>
2023-02-24 09:01:51 +0100

    fix: bug corrections

--- Views/LoadingViewGUI.xaml.cs
+++ Views/LoadingViewGUI.xaml.cs
@@ -57,58 +57,6 @@ namespace easysave.Views
         }
 
 
-        [DllImport("user32.dll")]
-        static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
-        [DllImport("user32.dll")]
-        static extern bool EnableMenuItem(IntPtr hMenu, uint uIDEnableItem, uint uEnable);
-
-
-
-        const uint MF_BYCOMMAND = 0x00000000;
-        const uint MF_GRAYED = 0x00000001;
-        const uint MF_ENABLED = 0x00000000;
-
-        const uint SC_CLOSE = 0xF060;
-
-        const int WM_SHOWWINDOW = 0x00000018;
-        const int WM_CLOSE = 0x10;
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        protected override void OnSourceInitialized(EventArgs e)
-        {
-            base.OnSourceInitialized(e);
-
-            HwndSource hwndSource = PresentationSource.FromVisual(this) as HwndSource;
-
-            if (hwndSource != null)
-            {
-                hwndSource.AddHook(new HwndSourceHook(this.hwndSourceHook));
-            }
-        }
-
-
-        IntPtr hwndSourceHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-        {
-            if (msg == WM_SHOWWINDOW)
-            {
-                IntPtr hMenu = GetSystemMenu(hwnd, false);
-                if (hMenu != IntPtr.Zero)
-                {
-                    EnableMenuItem(hMenu, SC_CLOSE, MF_BYCOMMAND | MF_GRAYED);
-                }
-            }
-            else if (msg == WM_CLOSE)
-            {
-                handled = false;
-            }
-            return IntPtr.Zero;
-        }
-
-
     public LoadingViewGUI(Loader loader)
         {
             InitializeComponent();
@@ -127,6 +75,10 @@ namespace easysave.Views
             items.Insert(0, item);
             listView.ItemsSource = null;
             listView.ItemsSource = items;
+            if(loader.getPercentage() == 100)
+            {
+                closeAllowed= true;
+            }
         }
 
         private void translateAllItems()
